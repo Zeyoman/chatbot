7386198e5a4004b633d363386ca9c34bfa323453
Zeyoman <alex10pump@gmail.com>
2024-06-28 14:18:27 +0200

    ajout pp

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var id = 0
+
 // fonction pour envoyer le message
 document.getElementById('send-button').addEventListener('click', async () => {
     const userInput = document.getElementById('user-input').value;
@@ -9,30 +11,50 @@ document.getElementById('send-button').addEventListener('click', async () => {
 
 // ajouté le message et différencié 
 function addMessage(sender, message) {
+    id ++
     const output = document.getElementById('output');
     const messageElement = document.createElement('div');
+    const containerMessage = document.createElement('div');
+    const imageBot = document.createElement('img');
+    imageBot.src = "robot.png";
+    imageBot.width = 20;
+    imageBot.height = 20;
     messageElement.classList.add('message');
+    containerMessage.classList.add('containerMessage');
     messageElement.textContent = message;
 
     if (sender === 'User') {
+        containerMessage.classList.add('container-user-message');
         messageElement.classList.add('user-message');
+        messageElement.id = id
+        output.appendChild(containerMessage);
+        console.log(id)
     } else {
+        containerMessage.classList.add('container-bot-message');
         messageElement.classList.add('bot-message');
+        messageElement.id = id
+        output.appendChild(containerMessage);
+        containerMessage.appendChild(imageBot);
+        console.log(messageElement)
     }
 
-
-    output.appendChild(messageElement);
+    containerMessage.appendChild(messageElement);
     output.scrollTop = output.scrollHeight;
 }
 
 // le code du chatbot
 async function respondToUser(userInput) {
     let response = '';
+    userInput = userInput.toLowerCase()
 
     if (userInput.includes('bonjour') || userInput.includes('hello') || userInput.includes('salut')) {
         response = 'Bonjour! Comment puis-je vous aider aujourd\'hui?';
     } else if (userInput.includes('bonsoir')) {
         response = 'Bonsoir! Comment puis-je vous aider aujourd\'hui?';
+    } else if (userInput.includes('ca va') && userInput.includes('?') || userInput.includes('ça va') && userInput.includes('?')) {
+        response = 'Je vais bien merci, et vous, comment allez vous ?';
+    } else if (userInput.includes('ca va') || userInput.includes('ça va') || userInput.includes('ça va')) {
+        response = 'Heureux de savoir que vous allez bien !';
     } else if (userInput.includes('aide')) {
         response = 'Je suis Kintsugi, un chatbot qui peut vous aider avec des questions sur la santé mentale et le soutien émotionnel. Comment puis-je vous aider aujourd\'hui?';
     } else if (userInput.includes('merci')) {
@@ -40,10 +62,10 @@ async function respondToUser(userInput) {
     } else if (userInput.includes('bye')) {
         response = 'Au revoir! Passez une bonne journée.';
     } else if (userInput.includes('dépression') || userInput.includes('triste') || userInput.includes('perdu') || userInput.includes('perdue') || userInput.includes('ennuyeux') || userInput.includes('angoisse') || userInput.includes('angoissant')) {
-        response = 'Je suis désolé que vous vous sentiez ainsi. La dépression et l\'anxiété sont des maladies mentales sérieuses. Il est important de parler à un professionnel de la santé. En attendant, vous pouvez essayer quelques techniques de relaxation : \n\n- **Méditation** : trouvez un endroit calme, asseyez-vous confortablement et concentrez-vous sur votre respiration pendant quelques minutes. \n Vous pouvez aussi trouver d\'autre méthode de relaxation dans l\'application les heures de kintsugi dans l\'onglet séances.';
+        response = 'Je suis désolé que vous vous sentiez ainsi. La dépression et l\'anxiété sont des maladies mentales sérieuses. Il est important de parler à un professionnel de la santé. En attendant, vous pouvez essayer quelques techniques de relaxation : \n\n- Méditation : trouvez un endroit calme, asseyez-vous confortablement et concentrez-vous sur votre respiration pendant quelques minutes. \n Vous pouvez aussi trouver d\'autre méthode de relaxation dans l\'application les heures de kintsugi dans l\'onglet séances.';
     } else if (userInput.includes('cancer')) {
         response = 'Le cancer est une maladie où des cellules anormales se divisent de manière incontrôlée et détruisent les tissus corporels. Des cancers ont déjà été guerie, ce n\'est donc pas fini. Nous sommes là pour vous aider à traverser cette période difficile.';
-    } else if (userInput.includes('relax')) {
+    } else if (userInput.includes('relax') || userInput.includes('concentr')) {
         response = 'Voici un exercice de respiration : inspirez profondément pendant 4 secondes, retenez votre souffle pendant 7 secondes, puis expirez lentement pendant 8 secondes. Répétez cela cinq fois. \n Vous pouvez aussi trouver d\'autre méthode de relaxation dans l\'application les heures de kintsugi dans l\'onglet séances.';
     } else if (userInput.includes('tribu')) {
         response = 'La tribu représente votre réseau de soutien social. Avez-vous parlé à vos amis ou à votre famille récemment ? Ils peuvent être une source de réconfort et de force.';
@@ -76,18 +98,16 @@ async function respondToUser(userInput) {
     } else if (userInput.includes('courage')) {
         response = 'Il faut beaucoup de courage pour affronter les défis de la vie. Vous êtes plus fort que vous ne le pensez et chaque petit pas compte.';
     } else if (userInput.includes('maladi')) {
-        response = 'Toute maladie est soignable, il faut juste garder espoir et se donner les moyens de réussir ce combat. Notre application les heures de kintsugi peux vous aider mentalement et physiquement grâce à nos progammes et professionnel de santé.';
+        response = 'Toute maladie est soignable, il faut juste garder espoir et se donner les moyens de réussir ce combat. Notre application les heures de kintsugi peut vous aider mentalement et physiquement grâce à nos programmes et professionnel de santé.';
     } else if (userInput.includes('kintsugi')) {
-        response = 'Nous sommes une association qui permet au personne ayant une maladie ou des questions d\'être mieux informer et accompagné durant son parcours.';
-    } else if (userInput.includes('kintsugi')) {
-        response = 'Nous sommes une association qui permet au personne ayant une maladie ou des questions d\'être mieux informer et accompagné durant son parcours.';
+        response = 'Nous sommes une association qui permet aux personnes ayant une maladie ou des questions d\'être mieux informé et accompagner durant son parcours.';
     } else if (userInput.includes('pas bien')) {
         response = 'Je suis désolé de savoir que vous n\'allez pas bien, que puis-je faire pour vous ?.';
     }else {
         response = 'Désolé, je ne suis pas sûr de votre demande. Pouvez-vous reformuler?';
     }
 
-    // **Yoga** : essayez quelques postures simples de yoga comme la pose de l\'enfant ou la pose du cadavre pour vous détendre.
+    // Yoga : essayez quelques postures simples de yoga comme la pose de l\'enfant ou la pose du cadavre pour vous détendre.
 
     addMessage('Bot', response);
 }
@@ -106,3 +126,12 @@ document.getElementById('user-input').addEventListener('keypress', async (e) => 
         await respondToUser(userInput);
     }
 });
+
+// Changement de Thème bouton
+document.querySelectorAll('.button-person').forEach(element => {
+    element.addEventListener('click', function(){
+        document.getElementById('active').removeAttribute('id')
+        element.id = 'active'
+    })    
+});
+
